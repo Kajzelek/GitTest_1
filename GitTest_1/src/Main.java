42291b6b35ebd686b8e43fd073bacb32433d4e2f
Kajzelek <patryk.cygnar@student.pk.edu.pl>
2023-10-18 22:47:21 +0200

    Updated main V1.4

--- GitTest_1/src/Main.java
+++ GitTest_1/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TEST #1 14/10/23");
         System.out.println("Modyfikacja maina");
+        System.out.println("Stashowa zmiana");
     }
 }
